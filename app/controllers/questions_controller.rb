b27a2ddda715586aee1f190c06b7f4bde18534b1
rosalinda belen <rosabelen.v0610@gmail.com>
2022-05-31 23:54:29 -0500

    modificaciones

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -4,15 +4,21 @@ class QuestionsController < ApplicationController
   end
 
   def ask
-
+    if params[:alert]
+      @alert = params[:alert]
+    end
   end
 
   def answer
-    @question = params[:answer_letters] 
+    @question = params[:answer_letter]
 
-    if params[:answer_letters] == "I am going to work"
+    if @question === ""
+      redirect_to ask_path(alert:"debe escribir en su casilla")
+    end
+
+    if @question == "I am going to work"
       @answer = "Great!"
-    elsif params[:answer_letters].end_with?("?")
+    elsif params[:answer_letter].end_with?("?")
       @answer = "Silly question, get dressed and go to work!."
     else 
       @answer = "I don't care, get dressed and go to work!"
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get "questions", to: "questions#index"
-  get "ask", to: "questions#ask"
-  get "answer", to: "questions#answer"
+  get "questions", to: "questions#index", as: :question
+  get "ask", to: "questions#ask", as: :ask
+  get "answer", to: "questions#answer", as: :answer
 end
